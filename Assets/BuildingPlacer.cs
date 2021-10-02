5a2ba551a4a85eecda4cc40e8cfea663c260a890
Mick M <mickwpmcdonnell@gmail.com>
2021-10-02 20:16:00 +1000

    Refactored code to be less of a garbage mess

---
+++ Assets/BuildingPlacer.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingPlacer : MonoBehaviour
+{
+    public Building buildingToBePlaced;
+    public Building LumberYard;
+    public Building House;
+    public Building FishingHut;
+    public Building Mine;
+
+    Vector3 spawnRotation;
+    public Vector3 SpawnRotation { get => spawnRotation; private set => spawnRotation = value; }
+
+
+    public Island island;
+    private void Awake()
+    {
+        island = GameObject.FindObjectOfType<Island>();
+    }
+
+    public void TryPlaceBuilding(Vector3 pos, Resource resource)
+    {
+        Vector3 rotToPlace = spawnRotation;
+        SpawnRotation = new Vector3(0, buildingToBePlaced.GetRandomRotation(), 0);
+        island.TryPlaceBuilding(buildingToBePlaced, pos, rotToPlace, resource);
+    }
+
+    public void SetBuildingHouse()
+    {
+        SetBuildingToBePlaced(House);
+    }
+    public void SetBuildingLumberyard()
+    {
+        SetBuildingToBePlaced(LumberYard);
+    }
+
+    public void SetBuildingFishingHut()
+    {
+        SetBuildingToBePlaced(FishingHut);
+    }
+    public void SetBuildingMine()
+    {
+        SetBuildingToBePlaced(Mine);
+    }
+
+    public event System.Action<Building> BuildingSetToBePlacedEvent;
+    void SetBuildingToBePlaced(Building b)
+    {
+        buildingToBePlaced = b;
+        SpawnRotation = new Vector3(0, b.GetRandomRotation(), 0);
+
+        BuildingSetToBePlacedEvent?.Invoke(b);
+    }
+
+
+    public bool CanBuildingBePlacedOnResource(RaycastHit raycastHit, out Vector3 placementLocation, out Resource placementResource)
+    {
+        placementResource = null;
+        placementLocation = Vector3.zero;
+        //eg. maybe only woodcutters can be placed in forests
+        //Check tag/layer from raycast hit
+        Resource resource = raycastHit.collider.gameObject.GetComponent<Resource>();
+        if (resource != null)
+        {
+            if (buildingToBePlaced.AllowedResoucePlacements.Contains(resource.ResourceType) == false) return false;
+            //check if we can place here then return
+            placementLocation = buildingToBePlaced.setResourceInactiveOnPlacement ? resource.transform.position : raycastHit.point;
+            placementResource = resource;
+            return true;
+        }
+
+        if (buildingToBePlaced.AllowedResoucePlacements.Count > 0 && buildingToBePlaced.AllowedResoucePlacements.Contains(ResourceType.NONE) == false) return false;
+
+        Building building = raycastHit.collider.gameObject.GetComponent<Building>();
+        if (building != null)
+        {
+            return false;
+        }
+
+        Island island = raycastHit.collider.gameObject.GetComponentInParent<Island>();
+        if (island != null && (buildingToBePlaced.locationPlacement == Building.IslandLocationPlacement.ALL || buildingToBePlaced.locationPlacement == Building.IslandLocationPlacement.GRASS))
+        {
+            //We can place stuff on the island
+            placementLocation = raycastHit.point;
+            return true;
+        }
+
+        Beach beach = raycastHit.collider.gameObject.GetComponentInParent<Beach>();
+        if (beach != null && (buildingToBePlaced.locationPlacement == Building.IslandLocationPlacement.ALL || buildingToBePlaced.locationPlacement == Building.IslandLocationPlacement.BEACH))
+        {
+            //We can place stuff on the island
+            placementLocation = raycastHit.point;
+            return true;
+        }
+
+        return false;
+    }
+}
--- Assets/MouseHandler.cs
+++ Assets/MouseHandler.cs
@@ -6,6 +6,32 @@ using UnityEngine.EventSystems;
 public class MouseHandler : MonoBehaviour
 {
     public MouseState mouseState;
+    BuildingPlacer buildingPlacer;
+    Island island;
+    private void Awake()
+    {
+        buildingPlacer = gameObject.GetComponent<BuildingPlacer>();
+        buildingPlacer.BuildingSetToBePlacedEvent += BuildingPlacer_BuildingSetToBePlacedEvent;
+        island = GameObject.FindObjectOfType<Island>();
+        island.BuildingPlacedEvent += Island_BuildingPlacedEvent;
+    }
+
+    private void Island_BuildingPlacedEvent(Building.BuildingType obj)
+    {
+        previewGO.transform.rotation = Quaternion.Euler(buildingPlacer.SpawnRotation);
+    }
+
+    private void BuildingPlacer_BuildingSetToBePlacedEvent(Building buildingToBePlaced)
+    {
+        mouseState = MouseState.BUILDING_PLACEMENT;
+        if (previewGO != null)
+            Destroy(previewGO.gameObject);
+
+        Building previewBuilding = Instantiate(buildingToBePlaced);
+        previewBuilding.SetAsPreview();
+        previewGO = previewBuilding.gameObject;
+        previewGO.transform.rotation = Quaternion.Euler(buildingPlacer.SpawnRotation);
+    }
 
     void Update()
     {
@@ -13,21 +39,21 @@ public class MouseHandler : MonoBehaviour
 
         if (Input.GetMouseButtonDown(1)) ReturnToDefaultState();
 
-        if (Input.GetKey(KeyCode.Alpha1))
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            SetBuildingHouse();
+            buildingPlacer.SetBuildingHouse();
         }
-        if (Input.GetKey(KeyCode.Alpha2))
+        if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            SetBuildingLumberyard();
+            buildingPlacer.SetBuildingLumberyard();
         }
-        if (Input.GetKey(KeyCode.Alpha3))
+        if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            SetBuildingFishingHut();
+            buildingPlacer.SetBuildingFishingHut();
         }
-        if (Input.GetKey(KeyCode.Alpha4))
+        if (Input.GetKeyDown(KeyCode.Alpha4))
         {
-            SetBuildingMine();
+            buildingPlacer.SetBuildingMine();
         }
 
         switch (mouseState)
@@ -45,7 +71,6 @@ public class MouseHandler : MonoBehaviour
     }
 
     GameObject previewGO;
-    Vector3 spawnRotation;
 
     void BuildingPlacementMouseStateUpdate()
     {
@@ -56,13 +81,13 @@ public class MouseHandler : MonoBehaviour
         {
             Vector3 placementLoc;
             Resource placementResource = null;
-            bool clearGround = CanBuildingBePlacedOnResource(raycastHit, out placementLoc, out placementResource);
+            bool clearGround = buildingPlacer.CanBuildingBePlacedOnResource(raycastHit, out placementLoc, out placementResource);
             //Can we place building
             if(clearGround)
             {
                 if (Input.GetMouseButtonDown(0))
                 {
-                    TryPlaceBuilding(placementLoc, placementResource); 
+                    buildingPlacer.TryPlaceBuilding(placementLoc, placementResource); 
                 } else
                 {
                     previewGO.SetActive(true);
@@ -82,97 +107,8 @@ public class MouseHandler : MonoBehaviour
             Destroy(previewGO.gameObject);
     }
 
-    public void SetBuildingHouse()
-    {
-        SetBuildingToBePlaced(House);
-    }
-    public void SetBuildingLumberyard()
-    {
-        SetBuildingToBePlaced(LumberYard);
-    }
-
-    public void SetBuildingFishingHut()
-    {
-        SetBuildingToBePlaced(FishingHut);
-    }
-    public void SetBuildingMine()
-    {
-        SetBuildingToBePlaced(Mine);
-    }
-
-    void SetBuildingToBePlaced(Building b)
-    {
-        mouseState = MouseState.BUILDING_PLACEMENT;
-        buildingToBePlaced = b; 
-        
-        if (previewGO != null)
-            Destroy(previewGO.gameObject);
-
-        Building previewBuilding = Instantiate(buildingToBePlaced);
-        previewBuilding.SetAsPreview();
-        previewGO = previewBuilding.gameObject;
-        spawnRotation = new Vector3(0,previewBuilding.GetRandomRotation(),0);
-        previewGO.transform.rotation = Quaternion.Euler(spawnRotation);
-    }
-
-    public Building buildingToBePlaced;
-    public Building LumberYard;
-    public Building House;
-    public Building FishingHut;
-    public Building Mine;
-
-    bool CanBuildingBePlacedOnResource(RaycastHit raycastHit, out Vector3 placementLocation, out Resource placementResource)
-    {
-        hoverObject = raycastHit.collider.gameObject;
-        placementResource = null;
-        placementLocation = Vector3.zero;
-        //eg. maybe only woodcutters can be placed in forests
-        //Check tag/layer from raycast hit
-        Resource resource = raycastHit.collider.gameObject.GetComponent<Resource>();
-        if (resource != null)
-        {
-            if (buildingToBePlaced.AllowedResoucePlacements.Contains(resource.ResourceType) == false) return false;
-            //check if we can place here then return
-            placementLocation = buildingToBePlaced.setResourceInactiveOnPlacement ? resource.transform.position : raycastHit.point;
-            placementResource = resource;
-            return true;
-        }
-
-        if (buildingToBePlaced.AllowedResoucePlacements.Count > 0 && buildingToBePlaced.AllowedResoucePlacements.Contains(ResourceType.NONE) == false) return false;
-
-        Building building = raycastHit.collider.gameObject.GetComponent<Building>();
-        if (building != null)
-        {
-            return false;
-        }
-
-        Island island = raycastHit.collider.gameObject.GetComponentInParent<Island>();
-        if (island != null && (buildingToBePlaced.locationPlacement == Building.IslandLocationPlacement.ALL || buildingToBePlaced.locationPlacement == Building.IslandLocationPlacement.GRASS))
-        {
-            //We can place stuff on the island
-            placementLocation = raycastHit.point;
-            return true;
-        }
-
-        Beach beach = raycastHit.collider.gameObject.GetComponentInParent<Beach>();
-        if (beach != null && (buildingToBePlaced.locationPlacement == Building.IslandLocationPlacement.ALL || buildingToBePlaced.locationPlacement == Building.IslandLocationPlacement.BEACH))
-        {
-            //We can place stuff on the island
-            placementLocation = raycastHit.point;
-            return true;
-        }
-
-        return false;
-    }
     public GameObject hoverObject;
 
-    public Island island;
-    void TryPlaceBuilding(Vector3 pos, Resource resource)
-    {
-        island.TryPlaceBuilding(buildingToBePlaced, pos, spawnRotation, resource);
-        spawnRotation = new Vector3(0, buildingToBePlaced.GetRandomRotation(), 0);
-        previewGO.transform.rotation = Quaternion.Euler(spawnRotation);
-    }
 
     void DefaultMouseStateUpdate()
     {
